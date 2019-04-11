10f0771f883b3b1f6f57b7a45bfe7601fafaa9df
root <396537452@qq.com>
2019-04-11 19:39:30 +0800

    Revert "LearnData调整为包内调用"
    This reverts commit 02ae1e2e

--- src/mechine_learning/LearnData.java
+++ src/mechine_learning/LearnData.java
@@ -1,25 +1,25 @@
 package mechine_learning;
 
-class LearnData {
+public class LearnData {
     private String path0;
     private String path1;
     private boolean similar;
 
-    LearnData(String path0, String path1, boolean similar) {
+    public LearnData(String path0, String path1, boolean similar) {
         this.path0 = path0;
         this.path1 = path1;
         this.similar = similar;
     }
 
-    String getPath0() {
+    public String getPath0() {
         return path0;
     }
 
-    String getPath1() {
+    public String getPath1() {
         return path1;
     }
 
-    boolean isSimilar() {
+    public boolean isSimilar() {
         return similar;
     }
 }
